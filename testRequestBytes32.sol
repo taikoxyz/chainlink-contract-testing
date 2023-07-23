fb7e038b2cddc0a25e0c84e0851a87c9b2cd1afe
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-07-23 16:49:44 -0400

    Create testRequestBytes32.sol

---
+++ testRequestBytes32.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract testRequestBytes32 is ChainlinkClient {
+
+    using Chainlink for Chainlink.Request;
+
+    address constant oracleTaiko = 0x7053af6475b2a11Bff65E697E349d66e6580d371;
+    string constant jobIdTaiko = "e0f10647ce9d47bb8b7e0d3f9ac97b60";
+    uint256 public constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10; // 0.1 * 10**18 (0.1 LINK)
+    bytes32 public currentPrice;
+
+    event RequestEthereumPriceFulfilled(
+        bytes32 indexed requestId,
+        bytes32 indexed price
+    );
+
+    constructor() {
+        setChainlinkToken(0xeC9a237864f7e78fd835Db717DB4e3d3c4254b11);
+    }
+
+    function requestEthereumPrice() public {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            stringToBytes32(jobIdTaiko),
+            address(this),
+            this.fulfillEthereumPrice.selector
+        );
+        req.add(
+            "get",
+            "https://marcuswentz.github.io/chainlink_test_json_url_types/"
+        );
+        req.add("path", "bytes32");
+        //req.addInt("times", 100);
+        sendChainlinkRequestTo(oracleTaiko, req, ORACLE_PAYMENT);
+    }
+
+    function fulfillEthereumPrice(
+        bytes32 _requestId,
+        bytes calldata _id
+    ) public recordChainlinkFulfillment(_requestId) {
+        bytes32 bytes32Id = bytes32(_id);
+        emit RequestEthereumPriceFulfilled(_requestId, bytes32Id);
+        currentPrice = bytes32Id;
+    }
+
+    function stringToBytes32(
+        string memory source
+    ) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            // solhint-disable-line no-inline-assembly
+            result := mload(add(source, 32))
+        }
+    }
+}
